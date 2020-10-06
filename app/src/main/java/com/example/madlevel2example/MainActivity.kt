bba610d3484b1c198dddcef1e987e2b8755aca12
A.W. Kritikos <anton.kritikos@hva.nl>
2020-10-06 13:52:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.madlevel2example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2example.databinding.ActivityMainBinding
+import com.example.madlevel2example.databinding.ItemReminderBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+    // Don't forget to create a binding object as you did in previous assignments.
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        // Create an onClickListener associated with the button
+        binding.btnAddReminder.setOnClickListener {
+            val reminder = binding.etReminder.text.toString()
+            addReminder(reminder)
+        }
+
+        // Initialize the recycler view with a linear layout manager, adapter
+        binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
+    }
+
+    private fun addReminder(reminder: String) {
+        if (reminder.isNotBlank()) {
+            reminders.add(Reminder(reminder))
+            reminderAdapter.notifyDataSetChanged()
+            binding.etReminder.text?.clear()
+        } else {
+            Snackbar.make(etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+}
+
+data class Reminder(
+        var reminderText: String
+
+)
+class ReminderAdapter(private val reminders: List<Reminder>) : RecyclerView.Adapter<ReminderAdapter.ViewHolder>(){
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val binding = ItemReminderBinding.bind(itemView)
+
+        fun databind(reminder: Reminder) {
+            binding.tvReminder.text = reminder.reminderText
+        }
+    }
+
+    /**
+     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_reminder, parent, false)
+        )
+    }
+
+    /**
+     * Returns the size of the list
+     */
+    override fun getItemCount(): Int {
+        return reminders.size
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(reminders[position])
+    }
+
+
+}
